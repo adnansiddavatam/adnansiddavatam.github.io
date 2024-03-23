1277e671bce6aa7f66cb4124cbe8c61b76b844ba
Adnan Siddavatam <adnan.ahmed.siddavatam@gmail.com>
2024-03-23 01:00:40 -0600

    buttons
    changed the buttons to icons

--- website/src/components/Buttons.js
+++ website/src/components/Buttons.js
@@ -1,13 +1,33 @@
 // src/components/Buttons.js
 import React from 'react';
 import styles from '../styles/Buttons.module.css';
+import githubLogo from '../components/Pictures/github.png';
+import resumeLogo from '../components/Pictures/resume.png';
+import contactLogo from '../components/Pictures/linkedin.png';
 
 function Buttons() {
   return (
+    
     <div className={styles.buttons}>
-      <button className={`${styles.button} ${styles.resume}`}>Resume</button>
-      <button className={`${styles.button} ${styles.projects}`}>Projects</button>
-      <button className={`${styles.button} ${styles.contact}` }>Contact</button>
+      <button className={`${styles.button} ${styles.github}`}>
+        <a href="https://github.com/adnansiddavatam" target="_blank" rel="noopener noreferrer" title="GitHub">
+          <div className={styles.githubCircle}>
+            <img src={githubLogo} alt="GitHub" style={{ height: '40px', width: '40px' }} />
+          </div>
+        </a>
+      </button>
+
+      <button className={`${styles.button} ${styles.resume}`}>
+        <img src={resumeLogo} alt="Resume" style={{ height: '40px', width: '40px' }} />
+      </button>
+    
+      <button className={`${styles.button} ${styles.contact}`}>
+        <a href="https://www.linkedin.com/in/adnan-siddavatam/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+          <div className={styles.contactCircle}>
+            <img src={contactLogo} alt="LinkedIn" style={{ height: '40px', width: '40px' }} />
+          </div>
+        </a>
+      </button>
     </div>
   );
 }
